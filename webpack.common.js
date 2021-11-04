03b07af1568ef860f91552caca6347fbadb58d6e
Vincent Zhong <vincentzhongca@gmail.com>
2021-11-03 22:37:15 -0400

    update webpack babel loader

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env"] },
+        options: { presets:  ["@babel/preset-env", "@babel/preset-react"] },
       },
       {
         test: /\.css$/,
